ecb54ed477a6e730df7aa642539eb8565ad968df
muktarkabir <mukhtarkabir3394@gmail.com>
2025-09-23 20:42:57 +0100

    Fix imports.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import "./reset.css";
 import "./global-styles.css";
 import "./styles/waitlist.css";
+import "./styles/hero.css";
+import "./styles/about.css";
+import "./styles/how-it-works.css";
+import "./styles/mission.css";
+import "./styles/gallery.css";
+import "./styles/impact.css";
+import "./styles/testimonials.css";
+import "./styles/faq.css";
 import { joinWaitlist, listenForWaitlistCount } from "./modules/firebase.js";
 import { emailIsValid, form, emailField } from "./modules/dom-stuff.js";
 
