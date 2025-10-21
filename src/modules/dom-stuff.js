b4aa6b5c0f84138df9bb3d0e6cb2c03461a26e30
muktarkabir <mukhtarkabir3394@gmail.com>
2025-10-21 14:46:32 +0100

    Update team member cards to include social links.

--- src/modules/dom-stuff.js
+++ src/modules/dom-stuff.js
@@ -101,14 +101,11 @@ export const giveFeedBack = (successful) => {
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      console.log(entry.target);
       if (entry.isIntersecting) {
         entry.target.classList.add("show");
       } else {
         entry.target.classList.remove("show");
       }
-      // entry.target.classList.toggle("show", entry.isIntersecting);
-      // observer.unobserve(entry.target);
     });
   },
   {
